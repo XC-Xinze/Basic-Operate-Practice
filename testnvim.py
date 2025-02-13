9764a9a2862f69576d655646040b45e6ee1fe82a
XC-Xinze <xchen7@gradcenter.cuny.edu>
2025-02-12 20:53:47 -0500

    practice Nvim

---
+++ testnvim.py
@@ -0,0 +1,8 @@
+import math
+
+dictionary_test = {"bob": "Apple", "Alice": "orange", "Diana": "Honey"}
+print("hello")
+for name, fruit in dictionary_test.items():
+    print(f"{name.title()} likes eating {fruit.upper()}")
+    # This file is creating by Nvim
+    # Run in Virtual Environment
