176ad3b2a438e08d2f20a4dd169ddf14500d8cd2
XC-Xinze <xchen7@gradcenter.cuny.edu>
2025-02-14 13:32:57 -0500

    chapter 7

--- inputandwhile.py
+++ inputandwhile.py
@@ -11,3 +11,15 @@ prompt = int(prompt)
 if prompt >= 18:
     print("Damn! You are adult!")
 # % 求模运算符 same as C
+# 问卷调查练习
+response = {}
+mark_end = True
+while mark_end:
+    answer = input("What's your name?")
+    answer2 = input("How old are you?")
+    response[answer] = answer2
+    answer = input("If there any others? Yes / No:")
+    if answer == "No":
+        mark_end = False
+for name in response.items():
+    print("What's your name?", name)
