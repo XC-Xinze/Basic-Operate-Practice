fc4db900b474eaed3c979c5ac84551b5aa226ba5
XC-Xinze <xchen7@gradcenter.cuny.edu>
2025-02-16 21:26:54 -0500

    fix File practice 1

--- File.py
+++ File.py
@@ -10,9 +10,11 @@ print(contents)  # 这样就删除了额外的空行
 # 分割内容
 string_num = len(contents)
 print(string_num)
+print(repr(contents))
 contents = contents.splitlines()
 string_len = ""
 for line in contents:
     string_len += line
 string_num = len(string_len)
 print(string_num)
+print(repr(string_len))  # 可以看出,分割后换行符就消失了,不计入字符数
